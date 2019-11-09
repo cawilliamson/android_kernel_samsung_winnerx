031434a3e0b308f881ad11c207c826d51f58c6b4
Christopher A. Williamson <home@chrisaw.com>
2019-11-09 16:17:17 +0000

    Fix defconfig name

--- build_kernel.sh
+++ build_kernel.sh
@@ -9,7 +9,7 @@ KERNEL_LLVM_BIN="/opt/llvm-arm-toolchain-ship/6.0/bin/clang"
 CLANG_TRIPLE="aarch64-linux-gnu-"
 KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
 
-make -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV CFP_CC=$KERNEL_LLVM_BIN CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE winnerx_defconfig
+make -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV CFP_CC=$KERNEL_LLVM_BIN CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE winnerx_eur_open_defconfig
 make -j$(nproc --all) -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV CFP_CC=$KERNEL_LLVM_BIN CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE
 
 cp -v out/arch/arm64/boot/Image-dtb $(pwd)/arch/arm64/boot/Image-dtb
