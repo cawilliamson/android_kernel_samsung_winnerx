5ec0cd000ed29ea9caf5435a3a3aed8c6d9bebef
Christopher A. Williamson <home@chrisaw.com>
2019-11-22 21:27:26 +0000

    Fixed typo fuckup

--- drivers/clk/clk.c
+++ drivers/clk/clk.c
@@ -4896,7 +4896,16 @@ EXPORT_SYMBOL_GPL(of_clk_get_parent_name);
  * Return: number of parents for the clock node.
  */
 int of_clk_parent_fill(struct device_node *np, const char **parents,
-		       unsigned i#ifdef CONFIG_SEC_DEBUG
+		       unsigned int size)
+{
+	unsigned int i = 0;
+
+	while (i < size && (parents[i] = of_clk_get_parent_name(np, i)) != NULL)
+		i++;
+
+	return i;
+}
+EXPORT_SYMBOL_GPL(of_clk_parent_fill);
 
 struct clock_provider {
 	of_clk_init_cb_t clk_init_cb;
